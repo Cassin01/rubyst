15a91fb73d966fe8e6a299485defa26d3655fb01
Cassin01 <koyojb@gmail.com>
2019-01-27 03:15:17 +0900

    p39.rs

--- Rust/p39.rs
+++ Rust/p39.rs
@@ -1,11 +1,12 @@
 enum Tree {
-    Nil,
+    Lit(f64),
     Cons(String, Box<Tree>, Box<Tree>),
 }
 
+// 行きがけ順
 fn preorder(tree: &Tree) {
     match tree {
-        Tree::Nil => (),
+        Tree::Lit(_) => (),
         Tree::Cons(x, y, z) => {
             println!("{}", x);
             preorder(y);
@@ -14,38 +15,51 @@ fn preorder(tree: &Tree) {
     }
 }
 
-fn main() {
-    let node = Tree::Cons(
-                        String::from("節1"),
-                        Box::new(Tree::Cons(
-                            String::from("節2"),
-                            Box::new(Tree::Cons(
-                                String::from("葉A"),
-                                Box::new(Tree::Nil),
-                                Box::new(Tree::Nil)
-                            )),
-                            Box::new(Tree::Cons(
-                                String::from("葉B"),
-                                Box::new(Tree::Nil),
-                                Box::new(Tree::Nil)
-                            ))
-                            )
-                        ),
-                        Box::new(Tree::Cons(
-                            String::from("節3"),
-                            Box::new(Tree::Cons(
-                                String::from("葉C"),
-                                Box::new(Tree::Nil),
-                                Box::new(Tree::Nil)
-                            )),
-                            Box::new(Tree::Cons(
-                                String::from("葉D"),
-                                Box::new(Tree::Nil),
-                                Box::new(Tree::Nil)
-                            )),
-                            )
-                        )
-                );
-	preorder(&node);
+// 帰りがけ順
+fn postorder(tree: &Tree) {
+    match tree {
+        Tree::Lit(_) => (),
+        Tree::Cons(x, y, z) => {
+            postorder(y);
+            postorder(z);
+            println!("{}", x);
+        },
+    }
 }
 
+fn main() {
+    let node = Tree::Cons(
+                    String::from("節1"),
+                    Box::new(Tree::Cons(
+                        String::from("節2"),
+                        Box::new(Tree::Cons(
+                            String::from("葉A"),
+                            Box::new(Tree::Lit(1.0_f64)),
+                            Box::new(Tree::Lit(1.0_f64))
+                        )),
+                        Box::new(Tree::Cons(
+                            String::from("葉B"),
+                            Box::new(Tree::Lit(1.0_f64)),
+                            Box::new(Tree::Lit(1.0_f64))
+                        ))
+                        )
+                    ),
+                    Box::new(Tree::Cons(
+                        String::from("節3"),
+                        Box::new(Tree::Cons(
+                            String::from("葉C"),
+                            Box::new(Tree::Lit(1.0_f64)),
+                            Box::new(Tree::Lit(1.0_f64))
+                        )),
+                        Box::new(Tree::Cons(
+                            String::from("葉D"),
+                            Box::new(Tree::Lit(1.0_f64)),
+                            Box::new(Tree::Lit(1.0_f64))
+                        )),
+                        )
+                    )
+                );
+	preorder(&node);
+    println!("");
+    postorder(&node);
+}

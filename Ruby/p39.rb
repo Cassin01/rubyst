379c2bc5414dc03073251010715074d6981bd066
Cassin01 <koyojb@gmail.com>
2019-01-27 03:11:38 +0900

    preorder

--- Ruby/p39.rb
+++ Ruby/p39.rb
@@ -6,6 +6,17 @@ def preorder(tree)
   end
 end
 
+def postorder(tree)
+  if tree[0].start_with?("節")
+    postorder(tree[1])
+    postorder(tree[2])
+  end
+  p(tree[0])
+end
+
+
 node1 = ["節1", ["節2", ["葉A"], ["葉B"]], ["節3", ["葉C"], ["葉D"]]]
 
 preorder(node1)
+p("")
+postorder(node1)

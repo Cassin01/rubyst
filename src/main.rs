248e30f63ff7195f550433443fd90f2ac1dc2ce8
Cassin01 <koyojb@gmail.com>
2019-01-30 07:42:51 +0900

    update parser

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-mod parser;
 mod tree;
 mod is;
 mod interpreter;
 mod read_code;
 
-mod test_parser;
-use test_parser::parser;
+mod parser;
+use parser::parser;
 
 fn main() {
     let ast = parser(String::from(" 1 + 15 % (2 ** 3)"));
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,17 @@
 use super::tree::Tree;
 use super::tree::Op;
 use super::is;
+use std::iter::Peekable;
+use std::str::Chars;
+
+
+fn push_op(tree: Tree, op: String) -> Tree {
+    if tree.root == Op::Nil {
+        tree.root(op)
+    } else {
+        Tree::new(enum_op(op)).left(Some(Box::new(tree)))
+    }
+}
 
 fn enum_op(op: String) -> Op<String> {
     match op.as_str() {
@@ -11,32 +22,6 @@ fn enum_op(op: String) -> Op<String> {
         "%" => Op::Rem,
         "**" => Op::Pow,
         _   => panic!("not operator"),
-    }
-}
-
-fn push_num(mut tree: Tree, num: String) -> Tree {
-    if tree.root == Op::Nil {
-        tree.left(Some(Box::new(Tree::new(Op::Lit(num)))))
-    } else {
-        tree.push_back(num);
-        tree
-    }
-}
-
-fn push_tree(mut tree: Tree, insert_tree: Tree) -> Tree {
-    if tree.root == Op::Nil {
-        tree.left(Some(Box::new(insert_tree)))
-    } else {
-        tree.push_back_tree(insert_tree);
-        tree
-    }
-}
-
-fn push_op(tree: Tree, op: String) -> Tree {
-    if tree.root == Op::Nil {
-        tree.root(op)
-    } else {
-        Tree::new(enum_op(op)).left(Some(Box::new(tree)))
     }
 }
 
@@ -52,45 +37,120 @@ fn push_op_products(tree: Tree, op: String)
     }
 }
 
+fn push_num(mut tree: Tree, num: String) -> Tree {
+    if tree.root == Op::Nil {
+        tree.left(Some(Box::new(Tree::new(Op::Lit(num)))))
+    } else {
+        tree.push_back(num);
+        tree
+    }
+}
+
+fn is_operator_sums(s: &String) -> bool {
+    match s.as_str() {
+        "+" => true,
+        "-" => true,
+        _   => false,
+    }
+}
+
+fn is_operator_puroducts(s: &String) -> bool {
+    match s.as_str() {
+        "*" => true,
+        "/" => true,
+        "%" => true,
+        "**" => true,
+        _   => false,
+    }
+}
+
+fn push_tree(mut tree: Tree, insert_tree: Tree) -> Tree {
+    if tree.root == Op::Nil {
+        tree.left(Some(Box::new(insert_tree)))
+    } else {
+        tree.push_back_tree(insert_tree);
+        tree
+    }
+}
+
 pub fn parser(code: String) -> Tree {
     let mut num = String::new();
+    let mut op = String::new();
     let mut code_in_brackets = String::new();
-    let mut num_flag = false;
-    let mut not_brackets = true;
 
     // Abstract syntax tree
     let mut ast = Tree::new(Op::Nil);
 
-    for c in code.chars() {
-        if is::is_num(&c) && not_brackets {
-            num_flag = true;
-            num.push(c);
-        } else if is::is_space(&c) && not_brackets {
-            if num_flag {
-                ast = push_num(ast, num.clone());
-                num.clear();
-                num_flag = false;
+    let mut cs = code.chars().peekable();
+
+
+    loop {
+        match cs.peek() {
+            Some(_) => (),
+            None => break,
+        }
+
+        // 数字
+        if is_this(&mut cs, &is::is_num) {
+            while is_this(&mut cs, &is::is_num) {
+                if let Some(c) = cs.next() {
+                    num.push(c);
+                } else {
+                    break;
+                }
             }
-        } else if is::is_operator_sums(&c) && not_brackets{
-            num_flag = false;
-            ast = push_op(ast, c.to_string());
-        } else if is::is_operator_products(&c) && not_brackets {
-            num_flag = false;
-            ast = push_op_products(ast, c.to_string());
-        } else if is::is_first_bracket(&c) && not_brackets {
-            not_brackets = false;
-        } else if is::is_second_bracket(&c) {
+            ast = push_num(ast, num.clone());
+            num.clear();
+        }
+
+        // スペース
+        else if is_this(&mut cs, &is::is_space) {
+            while is_this(&mut cs, &is::is_space) {
+                if let Some(_) = cs.next() {
+                } else {
+                    break;
+                }
+            }
+        }
+
+        // 演算子
+        else if is_this(&mut cs, &is::is_operator) {
+            while is_this(&mut cs, &is::is_operator) {
+                if let Some(c) = cs.next() {
+                    op.push(c);
+                } else {
+                    break;
+                }
+            }
+            if is_operator_sums(&op) {
+                ast = push_op(ast, op.clone());
+            } else if is_operator_puroducts(&op) {
+                ast = push_op_products(ast, op.clone());
+            }
+            op.clear();
+        }
+
+        // 括弧
+        else if is_this(&mut cs, &is::is_first_bracket) {
+            cs.next();
+            while !is_this(&mut cs, &is::is_second_bracket) {
+                if let Some(c) = cs.next() {
+                    code_in_brackets.push(c);
+                } else {
+                    break;
+                }
+            }
+            cs.next();
             ast = push_tree(ast, parser(code_in_brackets.clone()));
             code_in_brackets.clear();
-            not_brackets = true;
-        } else if !not_brackets {
-            code_in_brackets.push(c);
-        } else {
         }
     }
-    if num_flag {
-        ast = push_num(ast, num.clone());
-        num.clear();
-    }
     ast
+}
+
+fn is_this(cs: &mut Peekable<Chars>, f: &Fn(&char)->bool) -> bool {
+    match cs.peek() {
+        Some(c) => f(&c),
+        None => false,
+    }
 }

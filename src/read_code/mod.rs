0cf3141c83b2db122baa15833b0afc88a2ae4456
Cassin01 <koyojb@gmail.com>
2019-02-01 03:08:18 +0900

    enable to read Multiple lines

--- src/read_code/mod.rs
+++ src/read_code/mod.rs
@@ -1,18 +1,15 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::io::prelude::*;
 
 pub fn read_code() -> String {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let file = File::open(filename).expect("file not found");
+    let mut file = File::open(filename).expect("file not found");
 
     let mut texts = String::new();
-    for line in BufReader::new(file).lines() {
-        texts.push_str(&line.unwrap().to_owned());
-        /*
-        texts.push_str(&line.unwrap().trim_right().to_owned());
-        */
-    }
+    file.read_to_string(&mut texts)
+        .expect("something went wrong reading the file");
     texts
 }
